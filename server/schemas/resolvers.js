604f47a0bda3842aa71ecddb35771a46e030445a
poisoned-eden <poisoned.eden@gmail.com>
2023-09-04 16:38:17 +0100

    adds first query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,20 @@
-const resolvers = {
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models'); 
 
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if ( context.user) {
+                const userData = await User
+                    .findOne({_id: context.user._id})
+                    .select('-__v -password');
+                
+                return userData
+            }
+
+            throw new AuthenticationError('No user logged in.');
+        }
+    }
 };
 
 module.exports = resolvers;
