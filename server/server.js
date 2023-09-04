bf8b1e5d9f184fb134fac7893f8de762a0f6b5a2
poisoned-eden <poisoned.eden@gmail.com>
2023-09-04 21:59:03 +0100

    switches properly to apollo

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-// TODO remove when built queries and mutations
-const routes = require('./routes');
+const { resolvers, typeDefs } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,15 +20,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// TODO remove when built queries and mutations
-app.use(routes);
-
-// TODO add the following code once you have built the queries and mutations in the client folder
-/* 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 const startApolloServer = async () => {
   await server.start();
@@ -42,13 +35,5 @@ const startApolloServer = async () => {
     });
   });
 };
-*/
 
-
-// TODO remove this code once you have built out queries and mutations in the client folder
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
-});
-
-// TODO add the following code once you have built the queries and mutations in the client folder
-// startApolloServer();
+startApolloServer();
