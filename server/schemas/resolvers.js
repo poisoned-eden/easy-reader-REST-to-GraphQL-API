c731e170bf723cd53a959c5e1e75ff9fb203bb72
poisoned-eden <poisoned.eden@gmail.com>
2023-09-04 21:49:59 +0100

    adds mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,48 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models'); 
+const { User } = require('../models');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if ( context.user) {
-                const userData = await User
-                    .findOne({_id: context.user._id})
-                    .select('-__v -password');
-                
-                return userData
-            }
+	Query: {
+		me: async (parent, args, context) => {
+			if (context.user) {
+				const userData = await User.findOne({
+					_id: context.user._id,
+				}).select('-__v -password');
 
-            throw new AuthenticationError('No user logged in.');
+				return userData;
+			}
+
+			throw new AuthenticationError('No user logged in.');
+		},
+	},
+
+	Mutation: {
+		addUser: async (parent, args) => {
+			const userData = await User.create(args);
+			const token = signToken(userData);
+
+			return { token, userData };
+		},
+		login: async (parent, args) => {
+			const user = await User.findOne({
+				$or: [{ username: args.username }, { email: args.email }],
+			});
+			if (!user) {
+				throw new AuthenticationError('Cant find this user');
+			}
+
+			const correctPw = await user.isCorrectPassword(args.password);
+
+			if (!correctPw) {
+				throw new AuthenticationError('incorrect password');
+			}
+			const token = signToken(user);
+			return { token, user };
+		},
+        saveBook: async (parent, args) => {
+            con
         }
-    }
+	},
 };
 
 module.exports = resolvers;
--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -6,7 +6,7 @@ const typeDefs = gql`
         username: String!
         email: String!
         bookCount: Int
-        savedBooks: [Book]
+        savedBooks: [Book!]
     }
 
     type Book {
@@ -28,8 +28,21 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+    
+    input BookInput {
+        bookId: ID!
+        authors: [String]
+        descrition: String!
+        title: String!
+        image: String
+        link: String        
     }
 `;
+
 
 module.exports = typeDefs;
