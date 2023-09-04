cd4c24b76ed88f3ebd7e40370abf5063e5203d55
poisoned-eden <poisoned.eden@gmail.com>
2023-09-04 16:54:11 +0100

    adds typeDefs for models, auth, query me

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,7 +1,35 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        descrition: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        
+    }
 `;
 
 module.exports = typeDefs;
